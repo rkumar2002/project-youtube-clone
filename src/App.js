0a8aa55602d0e69f12e639476d544f4073a7f9ef
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2024-06-19 21:53:05 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // this boiler-plate code can be implemented using 'rafce' keyword
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
 
 const App = () => {
-  return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar/>
+            <Routes>
+                <Route path="/" exact element={<Feed/>}/>
+                <Route path="/video/:id" element ={<VideoDetail/>}/>
+                <Route path="/channel/:id" element ={<ChannelDetail/>}/>
+                <Route path="/search/:searchTerm" element ={<SearchFeed/>}/>    
+            </Routes>    
+        </Box>
+    </BrowserRouter>
 }
 export default App;
 
