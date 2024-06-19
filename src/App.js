67a9a111c8ef4e8f8b7aecba87dd45b08584208e
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2024-06-19 21:13:22 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// this boiler-plate code can be implemented using 'rafce' keyword
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+export default App;
+
