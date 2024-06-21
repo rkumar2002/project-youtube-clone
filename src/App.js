90266c402c8dc3095d03b0a415b51b8540e36a2c
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2024-06-21 17:00:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 // this boiler-plate code can be implemented using 'rafce' keyword
-import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+// import Navbar from './components/Navbar';  // using path intellisense and npm intellisense extension, we can dblclick on component name and then ctrl+space to directly import them 
+// import ChannelDetail from './components/ChannelDetail';
+// import Feed from './components/Feed';
+// import VideoDetail from './components/VideoDetail';
+// import SearchFeed from './components/SearchFeed';
 
 const App = () => {
     <BrowserRouter>
